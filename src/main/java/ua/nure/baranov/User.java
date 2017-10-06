8caad9102ea810dae2d48de9175be2c5c05a1643
Yevhenii-Baranov <yevhenii.baranov@nure.ua>
2017-10-06 17:38:58 +0300

    Multiple testfiles are now available, also Javadoc comments added

--- src/main/java/ua/nure/baranov/User.java
+++ src/main/java/ua/nure/baranov/User.java
@@ -3,60 +3,85 @@ package ua.nure.baranov;
 import java.util.Calendar;
 
 /**
- * @author effffgen
- *
+ * @author Yevhenii Baranov
  */
 public class User {
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private Calendar birthDay;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+	
+	/**
+	 * Setter for the first name of the user
+	 * @param firstName - must not be empty string
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+
+	/**
+	 * Setter for the last name of the user
+	 * @param lastName - must not be empty string
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public Calendar getBirthDay() {
 		return birthDay;
 	}
+
 	public void setBirthDay(Calendar birthDay) {
 		this.birthDay = birthDay;
 	}
-	public String getFullName() throws IllegalStateException{
-		StringBuilder fullName=new StringBuilder();
-		if(firstName==null||lastName==null) {
+	
+	/**
+	 * Assembles and returns full name of user. 
+	 * @return Full name.
+	 * @throws IllegalStateException if either first or last name is not set.
+	 */
+	public String getFullName() throws IllegalStateException {
+		StringBuilder fullName = new StringBuilder();
+		if (firstName == null || lastName == null) {
 			throw new IllegalStateException("something is missing");
 		}
 		fullName.append(firstName).append(' ').append(lastName);
 		return fullName.toString();
 	}
-	
-	
-	public int getAge() throws IllegalStateException{
-		Calendar currentDate=Calendar.getInstance();
-		if(birthDay.compareTo(currentDate)>=0) {
+
+	/**
+	 * Calculates current age of user, depending on his birthday.
+	 * @return Age of user.
+	 * @throws IllegalStateException
+	 */
+	public int getAge() throws IllegalStateException {
+		Calendar currentDate = Calendar.getInstance();
+		if (birthDay.compareTo(currentDate) >= 0) {
 			throw new IllegalStateException("User has not been born yet");
 		}
-		int age=currentDate.get(Calendar.YEAR)-birthDay.get(Calendar.YEAR);
-		if(currentDate.get(Calendar.MONTH)<birthDay.get(Calendar.MONTH)
-				||(currentDate.get(Calendar.MONTH)==birthDay.get(Calendar.MONTH) 
-				&& currentDate.get(Calendar.DAY_OF_MONTH)<=birthDay.get(Calendar.DAY_OF_MONTH))) {
+		int age = currentDate.get(Calendar.YEAR) - birthDay.get(Calendar.YEAR);
+		if (currentDate.get(Calendar.MONTH) < birthDay.get(Calendar.MONTH)
+				|| (currentDate.get(Calendar.MONTH) == birthDay.get(Calendar.MONTH)
+				&& currentDate.get(Calendar.DAY_OF_MONTH) <= birthDay.get(Calendar.DAY_OF_MONTH))) {
 			age--;
 		}
 		return age;
 	}
-	
+
 }
--- src/test/java/ua/nure/baranov/UserTest.java
+++ src/test/java/ua/nure/baranov/UserTest.java
@@ -2,65 +2,71 @@ package ua.nure.baranov;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-
 import java.util.Calendar;
-
 import org.junit.Before;
 import org.junit.Test;
 
-public class UserTest  {
+public class UserTest {
 	private static final int TEST_AGE = 20;
 	private static final int TEST_DAY = 23;
 	private static final int TEST_MONTH = Calendar.SEPTEMBER;
 	private static final int TEST_YEAR = 1997;
+	/**
+	 * Variable that is used to calculate date in future in order to send wrong data to the function
+	 */
+	private static final int FUTURE_YEAR_SHIFT = 20;
 	
 	private User user;
 	private Calendar today;
+
 	@Before
 	public void setUp() throws Exception {
-		user=new User();
-		today=Calendar.getInstance();
+		user = new User();
+		today = Calendar.getInstance();
 	}
-	
+
 	@Test
 	public void getFullNameTest() {
 		user.setFirstName("Yevhenii");
 		user.setLastName("Baranov");
 		assertEquals("Yevhenii Baranov", user.getFullName());
 	}
-	
-	@Test(expected=IllegalStateException.class)
+
+	@Test(expected = IllegalStateException.class)
 	public void getFullNameFirstNameUnsettedTest() {
 		user.setLastName("Yevhenii");
 		user.getFullName();
 		fail();
 	}
-	
-	@Test(expected=IllegalStateException.class)
+
+	@Test(expected = IllegalStateException.class)
 	public void getFullNameLastNameUnsettedTest() {
 		user.setLastName("Baranov");
 		user.getFullName();
 		fail();
 	}
-	
-	
-	//This test is valid until the September of 2k18, lol
+
+	/**
+	 * Test for age calculator, is valid only from September of 2017 to September of 2018.
+	 * TODO: rewrite if it starts to fail.
+	 */
 	@Test
 	public void getAgeTest() {
-		Calendar userBirthDay=Calendar.getInstance();
+		Calendar userBirthDay = Calendar.getInstance();
 		userBirthDay.set(TEST_YEAR, TEST_MONTH, TEST_DAY);
 		user.setBirthDay(userBirthDay);
-		assertEquals(TEST_AGE,user.getAge());
+		assertEquals(TEST_AGE, user.getAge());
 	}
-	
-	
-	@Test(expected=IllegalStateException.class)
+
+	@Test(expected = IllegalStateException.class)
 	public void testAgeWithIllegalDate() {
-		Calendar illegalBirthDay=Calendar.getInstance();
-		int invalidYear=today.get(Calendar.YEAR)+20;
+		Calendar illegalBirthDay = Calendar.getInstance();
+		int invalidYear = today.get(Calendar.YEAR) + FUTURE_YEAR_SHIFT;
 		illegalBirthDay.set(invalidYear, TEST_MONTH, TEST_DAY);
 		user.setBirthDay(illegalBirthDay);
 		user.getAge();
+		fail();
 	}
-	
+
+
 }
